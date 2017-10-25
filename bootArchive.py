aec07a25434a1ea8d7380cae52fe3a55ca47edca
sschatz1997 <s.schatz1997@gmail.com>
2017-10-25 18:22:49 -0400

    Add files via upload
    For this program to work it has to be in the boot folder (/etc/init.d/) with the boot script to run it (boot_script.sh).

---
+++ bootArchive.py
@@ -0,0 +1,14 @@
+# This file is for assurance purposes and to help us see when the two pi's conect.
+import socket
+from time import gmtime, strftime
+import os
+import time
+
+bootFile = "root/Desktop/boot.txt" #This can change based on how we decied to make the file system.
+bootFileVersion = 1
+file = open(bootFile, "w")
+file.write("The boot time was: " + time.strftime('%a %H:%M:%S %Y.') + '\n')
+time.sleep(0.5)
+os.systen("ping -c 1 127.0.0.1 >> boot.txt") #The IP address will be changed acourding to the pi's address.
+
+# once the syntax for the irdium network is found this part will sen this file.
