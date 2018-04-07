618ac961a74b06fc2fe035f1edee3f63fe01c51b
edemms <29679755+edemms@users.noreply.github.com>
2018-04-07 11:58:32 -0400

    Updated spacing and formatted strings

--- Proposed/Proposed Integration/LaserSetup.py
+++ Proposed/Proposed Integration/LaserSetup.py
@@ -1,16 +1,16 @@
-  import serial
-  from LaserCall import laser
-  ser = serial.Serial(port='/dev/ttyUSB2')
-  l = laser()
+import serial
+from LaserCall import laser
+ser = serial.Serial(port='/dev/ttyUSB2')
+l = laser()
 
-  while 1:
-      sendmessage(l.measure())
+while 1:
+    sendmessage(l.measure())
 
-  def sendMessage(m):
-      ser.write("AT")
-          if ser.inWaiting()>0:
-              ser.write(f"AT+SDBWRT({m})\r\n")
-              time.sleep(1)
-              ser.write("AT+SBDIX\r\n")
-              while ser.inWaiting()>0:
-                  out += ser.read(1)
+def sendMessage(m):
+    ser.write('AT')
+    if ser.inWaiting()>0:
+        ser.write('AT+SDBWRT({})\r\n'.format(m))
+        time.sleep(1)
+        ser.write('AT+SBDIX\r\n')
+        while ser.inWaiting()>0:
+            out += ser.read(1)
