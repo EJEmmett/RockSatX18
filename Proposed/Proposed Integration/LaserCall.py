ab236ac15a806feee8e34cee03c8b5d72ec24e1d
edemms <29679755+edemms@users.noreply.github.com>
2018-04-09 04:46:36 -0400

    Updated Things
    Added:
    Updated BaudRate
    Parity Bit
    Reduced sleep time
    Added Temperature function
    Added proper registry locations (distance:24, fc:4)(Temperature:22, fc:4)

--- Proposed/Proposed Integration/LaserCall.py
+++ Proposed/Proposed Integration/LaserCall.py
@@ -1,25 +1,37 @@
 import time
-import minmialmodbus as mini
+import minimalmodbus as mini
 
 class laser:
-    Switch = False
+    mini.BAUDRATE=115200
+    mini.PARITY='1'
     masterTime = time.strftime('%H:%M:%S')
 
-    primaryInstrument = mini.instrument("/dev/ttyUSB0")
-    secondaryInstrument = mini.instrument("/dev/ttyUSB1")
-    primaryPass = primaryInstrument.read_register()
-    secondaryPass = secondaryInstrument.read_register()
+    primaryInstrument = mini.instrument("/dev/ttyUSB1",1,mode='rtu')
+    secondaryInstrument = mini.instrument("/dev/ttyUSB2",2,mode='rtu')
 
     def __init__(self):
         self.primaryInstance = ""
         self.secondaryInstance = ""
     def measure(self):
-        if primaryPass != 0:
+        primaryPass = primaryInstrument.read_register(24, functioncode = 4)
+        secondaryPass = secondaryInstrument.read_register(24, functioncode = 4)
+
+        if primaryPass < 1000:
             primaryInstance = ('Instance occurred at: {}'.format(masterTime))
-            time.sleep(1)
-        if secondaryPass != 0:
+            time.sleep(.5)
+        if secondaryPass < 1000:
             secondaryInstance = ('Instance occurred at: {}'.format(masterTime))
-            time.sleep(1)
+            time.sleep(.5)
 
         combinedInstance = '{}|{}'.format(primaryInstance,secondaryInstance)
         return combinedInstance
+
+    def temperature(self):
+        primaryTemp = primaryInstrument.read_register(22,numberOfDecimals=2,functioncode=4)
+        secondaryTemp = secondaryInstrument.read_register(22,numberOfDecimals=2,functioncode=4)
+
+        primaryTemp = primaryTemp*(9/5)+32
+        seondaryTemp = secondaryTemp*(9/5)+32
+
+        combinedTemp = '{}|{}'.format(primaryTemp,secondaryTemp)
+        return combinedTemp
