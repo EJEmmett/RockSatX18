0d667c1090ab724fedcf1c737ff61e234e0d0aa6
sschatz1997 <s.schatz1997@gmail.com>
2018-04-10 13:24:40 -0400

    Update LaserSetup.py
    Fixed organization.

--- Proposed/Proposed Integration/LaserSetup.py
+++ Proposed/Proposed Integration/LaserSetup.py
@@ -5,17 +5,22 @@ from time import sleep
 ser = serial.Serial(port='/dev/ttyUSB0',19200)
 l = laser()
 out = ""
+
 while 1:
+    
     if l.measure():
         sendmessage(l.measure())
+        
     ser.reset_output_buffer()
 
 
 def sendMessage(m):
     ser.write('AT')
+    
     if ser.inWaiting()>0:
         ser.write('AT+SDBWRT({})\r\n'.format(m).encode('utf-8'))
         sleep(1)
         ser.write('AT+SBDIX\r\n'.encode('utf-8'))
+        
         if ser.in_waiting!=0:
             ser.reset_input_buffer()
