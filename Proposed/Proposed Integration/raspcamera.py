8b4ab5760fdfde68b46baf37ce1989c4637c3a66
edemms <29679755+edemms@users.noreply.github.com>
2018-04-11 03:36:54 -0400

    Create raspcamera.py

---
+++ Proposed/Proposed Integration/raspcamera.py
@@ -0,0 +1,12 @@
+import picamera
+from time import sleep
+max = 125
+
+sleep(125)
+with picamera.PiCamera() as camera:
+    camera.exposure_mode = 'antishake'
+    while(max!=0):
+        camera.capture_continuous('{timestamp:%H%M%S}-{counter:03d}.png')
+        sleep(7)
+        max-=1
+    break
