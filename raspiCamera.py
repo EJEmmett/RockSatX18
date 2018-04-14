4a77413e38905ef21370073e28bcb8c128ab6ab5
sschatz1997 <s.schatz1997@gmail.com>
2018-04-14 14:36:48 -0400

    Update raspiCamera.py

--- raspiCamera.py
+++ raspiCamera.py
@@ -1,6 +1,5 @@
 import picamera
 from time import sleep
-import os
 camera = picamera.PiCamera()
 pictureFileName = "pic#.jpg"
 outputVersion = 1
@@ -8,27 +7,13 @@ index = 0
 max = 125
 
 
-camera.sharpness = 0
-camera.contrast = 0
-camera.brightness = 50
-camera.saturation = 0
-camera.ISO = 0
-camera.video_stabilization = False
-camera.exposure_compensation = 0
 camera.exposure_mode = 'antishake'
-camera.meter_mode = 'average'
-camera.awb_mode = 'auto'
-camera.image_effect = 'none'
-camera.color_effects = None
-camera.rotation = 0
-camera.hflip = False
-camera.vflip = False
-camera.crop = (0.0, 0.0, 1.0, 1.0)
 
 sleep(125)
 while(index < max):
     pictureFileName1 = pictureFileName.replace("#", str(outputVersion))
-    camera.capture(pictureFileName1)
+    camera.capture_continuous(pictureFileName1)
     index += 1
     outputVersion += 1
     sleep(7)
+break
