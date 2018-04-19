0066cc80eed049a96f9aa95e45b1a55c76cea5c3
sschatz1997 <s.schatz1997@gmail.com>
2018-04-19 11:05:11 -0400

    Update IridiumMain.py
    Fixed formatting

--- Primary Cubesat/IridiumMain.py
+++ Primary Cubesat/IridiumMain.py
@@ -6,13 +6,13 @@ ser = serial.Serial(port='/dev/ttyUSB0',19200)
 
 def sendMessage(m):
     ser.write('AT+')
-    if ser.out_waiting!=0:
+    if ser.out_waiting !=0:
         ser.reset_input_buffer()
         ser.reset_output_buffer()
         ser.write('AT+SDBWRT({})\r\n'.format(m).encode('utf-8'))
         sleep(1)
         ser.write('AT+SBDIX\r\n'.encode('utf-8'))
-        if ser.out_waiting!=0:
+        if ser.out_waiting !=0:
             ser.reset_input_buffer()
             ser.reset_output_buffer()
 
