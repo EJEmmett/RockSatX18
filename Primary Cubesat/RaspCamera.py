3ebdac96787cea26722f5cfa7f935d443a81694f
sschatz1997 <s.schatz1997@gmail.com>
2018-04-19 11:06:24 -0400

    Update RaspCamera.py
    Fixed formatting

--- Primary Cubesat/RaspCamera.py
+++ Primary Cubesat/RaspCamera.py
@@ -5,7 +5,7 @@ max = 125
 sleep(125)
 with picamera.PiCamera() as camera:
     camera.exposure_mode = 'antishake'
-    while(max!=0):
+    while(max != 0):
         camera.capture_continuous('{timestamp:%H%M%S}-{counter:03d}.png')
         sleep(7)
         max-=1
