7ffd7333dc946abb4f06285652b61928f2647c16
edemms <29679755+edemms@users.noreply.github.com>
2018-04-21 16:27:07 -0400

    Updated Iridium
    Now use multiprocessing to buffer laser inputs

--- Primary Cubesat/IridiumMain.py
+++ Primary Cubesat/IridiumMain.py
@@ -1,8 +1,11 @@
-import serial
 import classModule
 from time import sleep
+from multiprocessing import Process, SimpleQueue
 iridium = classModule.Iridium()
 
 with classModule.Lasers() as lasers:
+    q = SimpleQueue()
+    laserList = Process(target=lasers.measure, args=(q,))
+    laserList.start()
     while 1:
-        iridium.sendMessage(lasers.measure())
+        iridium.sendMessage(q.get())
