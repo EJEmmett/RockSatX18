3b98a933fd45b51d2533647f8efcb1cb426e48c9
edemms <edemms12@gmail.com>
2018-05-15 08:08:39 -0400

    servo

---
+++ Primary Cubesat/servo.py
@@ -0,0 +1,24 @@
+import time
+import RPi.GPIO as GPIO
+
+# Set mode to physical pin slot
+GPIO.setmode(GPIO.BOARD)
+# Assign Pin 7 as output
+GPIO.setup(7, GPIO.OUT)
+
+#50 indicates HZ signal
+#Period of signal is 20 milliseconds
+p = GPIO.PWM(7, 50)
+#DutyCycle = PulseWidth/(1/frequency) = PulseWidth * frequency
+p.start(5)
+
+while True:
+    # Change to open
+    p.ChangeDutyCycle(10)
+    #wait for open
+    time.sleep(.4)
+    # Return to closed
+    p.ChangeDutyCycle(5)
+
+p.stop()
+GPIO.cleanup()
