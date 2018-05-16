0f6eedc1fa432630c6717be3b0e4d8637720f274
sschatz1997 <s.schatz1997@gmail.com>
2018-05-16 01:11:42 -0400

    Update music.py
    fixed bugs (maybe)

--- Music/music.py
+++ Music/music.py
@@ -3,36 +3,43 @@ from pygame import mixer
 import time
 from time import sleep as s
 
-mixer.intit
-mixer.music.load('/home/pi/Music/')
-mixer.music.play()
-s(300)
-mixer.quit()#stops the music could also be .stop()
+file1 = '/home/pi/Music/'
+file2 = '/home/pi/Music/'
+file3 = 'home/pi/Music/hoe_cakes'
+file4 = '/home/pi/Music/'
+file5 = '/home/pi/Music/'
 
 
 mixer.intit
-mixer.music.load('/home/pi/Music/')
-mixer.music.play()
+mixer.music.load(file1)
+mixer.music.play(0)
 s(300)
-mixer.quit()
+mixer.stop()#stops the music could also be .stop()
 
 
 mixer.intit
-mixer.music.load('/home/pi/Music/hoe_cakes.mp3')#the ONE rap song...
-mixer.music.play()
+mixer.music.load(file2)
+mixer.music.play(0)
 s(300)
-mixer.quit()
+mixer.stop()
+
 
 mixer.intit
-mixer.music.load('/home/pi/Music/')
-mixer.music.play()
+mixer.music.load(file3)#the ONE rap song...
+mixer.music.play(0)
 s(300)
-mixer.quit()
+mixer.stop()
 
 mixer.intit
-mixer.music.load('/home/pi/Music/')
-mixer.music.play()
+mixer.music.load(file4)
+mixer.music.play(0)
 s(300)
-mixer.quit()
+mixer.stop()
+
+mixer.intit
+mixer.music.load(file5)
+mixer.music.play(0)
+s(300)
+mixer.stop()
 
 printf("finished")
