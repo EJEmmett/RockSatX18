b8bfe33d63427d419650160a6c7dd8a66bf650fb
sschatz1997 <s.schatz1997@gmail.com>
2018-05-15 23:59:47 -0400

    Add files via upload
    The music test

---
+++ Tests/musicTest.py
@@ -0,0 +1,11 @@
+import pygame 
+from pygame import mixer
+import time
+from time import sleep as s
+
+
+mixer.intit
+mixer.music.load('/home/pi/Music/hoe_cakes.mp3')#the ONE rap song...
+mixer.music.play()
+s(300)
+mixer.quit()
