03d9ea99c1614e565361c11f29b1666d52704f53
sschatz1997 <s.schatz1997@gmail.com>
2018-05-15 23:07:34 -0400

    Update music.py
    add this peice

--- Music/music.py
+++ Music/music.py
@@ -1,1 +1,38 @@
-1
+import pygame 
+from pygame import mixer
+import time
+from time import sleep as s
+
+mixer.intit
+mixer.music.load('/home/pi/Music/')
+mixer.music.play()
+s(300)
+mixer.quit()#stops the music could also be .stop()
+
+
+mixer.intit
+mixer.music.load('/home/pi/Music/')
+mixer.music.play()
+s(300)
+mixer.quit()
+
+
+mixer.intit
+mixer.music.load('/home/pi/Music/')
+mixer.music.play()
+s(300)
+mixer.quit()
+
+mixer.intit
+mixer.music.load('/home/pi/Music/')
+mixer.music.play()
+s(300)
+mixer.quit()
+
+mixer.intit
+mixer.music.load('/home/pi/Music/')
+mixer.music.play()
+s(300)
+mixer.quit()
+
+printf("finished")
