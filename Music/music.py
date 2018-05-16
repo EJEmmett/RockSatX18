4271fc816b586e07737b9a1b2c8a5a2b83a5e699
edemms <edemms12@gmail.com>
2018-05-16 01:24:13 -0400

    fix

--- Music/music.py
+++ Music/music.py
@@ -1,6 +1,4 @@
-import pygame 
 from pygame import mixer
-import time
 from time import sleep as s
 
 file1 = '/home/pi/Music/'
@@ -9,37 +7,26 @@ file3 = 'home/pi/Music/hoe_cakes'
 file4 = '/home/pi/Music/'
 file5 = '/home/pi/Music/'
 
+mixer.init()
 
-mixer.intit
 mixer.music.load(file1)
-mixer.music.play(0)
-s(300)
-mixer.stop()#stops the music could also be .stop()
+mixer.music.play()
+s(180)
 
-
-mixer.intit
 mixer.music.load(file2)
-mixer.music.play(0)
-s(300)
-mixer.stop()
+mixer.music.play()
+s(180)
 
-
-mixer.intit
 mixer.music.load(file3)#the ONE rap song...
-mixer.music.play(0)
-s(300)
-mixer.stop()
+mixer.music.play()
+s(180)
 
-mixer.intit
 mixer.music.load(file4)
-mixer.music.play(0)
-s(300)
+mixer.music.play()
+s(180)
 mixer.stop()
 
-mixer.intit
 mixer.music.load(file5)
-mixer.music.play(0)
-s(300)
-mixer.stop()
+mixer.music.play()
 
 print("finished")
