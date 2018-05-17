1c3e62ff4283ca2294f6116e9c7011a548387b58
edemms <29679755+edemms@users.noreply.github.com>
2018-05-16 20:23:24 -0400

    fix2.0

--- Tests/musicTest.py
+++ Tests/musicTest.py
@@ -1,7 +1,7 @@
 from pygame import mixer
 from time import sleep as s
 
-file = 'file1 = '/home/pi/Music/hoe_cakes.mp3'
+file = '/home/pi/Music/hoe_cakes.mp3'
 
 mixer.init()
 mixer.music.load(file)#the ONE rap song...
