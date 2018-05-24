2c1009aca8372630b046ff87247a8a07441b8857
sschatz1997 <s.schatz1997@gmail.com>
2018-05-24 02:14:08 -0400

    Update servo.py
    fixes

--- Secondary Cubesat/servo.py
+++ Secondary Cubesat/servo.py
@@ -2,29 +2,31 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 def main():
-    i=0
+    i = 0
     #sleep(79)
 
     # Set mode to physical pin slot
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
     # Assign Pin 7 as output
-    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(18, GPIO.OUT)
 
     #50 indicates HZ signal
     #Period of signal is 20 milliseconds
-    p = GPIO.PWM(12, 50)
+    p=GPIO.PWM(18, 50)
     #DutyCycle = PulseWidth/(1/frequency) = PulseWidth * frequency
-    p.start(5)
+    p.start(7.5)
 
-    while i < 10:
+    while (i < 10):
         # Change to open
-        p.ChangeDutyCycle(10)
+        p.ChangeDutyCycle(7.5)
         #wait for open
-        sleep(15)#this is the time that I thought we decided on?
+        sleep(1)#this is the time that I thought we decided on?
         # Return to closed
-        p.ChangeDutyCycle(5)
-        i = i + 1
-
+        p.ChangeDutyCycle(10)
+        sleep(1)
+        p.ChangeDutyCycle(2.5)
+        i += 1
     p.stop()
     GPIO.cleanup()
 
