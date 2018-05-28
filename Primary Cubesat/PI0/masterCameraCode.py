64dcd8124d145be582c62a5ee81702f813bf79ac
sschatz1997 <s.schatz1997@gmail.com>
2018-05-28 18:44:56 -0400

    Update masterCameraCode.py
    added master log

--- Primary Cubesat/PI0/masterCameraCode.py
+++ Primary Cubesat/PI0/masterCameraCode.py
@@ -6,9 +6,15 @@ index = 0
 max = 35
 camera.exposure_mode = 'antishake'
 
+log = ["The camera took a picture at: "]
+o = open("masterLog.txt", "a+")
+
 sleep(2)
 while(index < max):
     camera.capture("pic" + outputVersion + ".jpg")
+    o.write(log[1] + time.strftime('%H:%M:%S') + '\n')
     index += 1
     outputVersion += 1
     sleep(7)
+
+    o.close()
