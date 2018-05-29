cb9783822bd067538188feac95a6a8209360984f
sschatz1997 <s.schatz1997@gmail.com>
2018-05-28 20:21:23 -0400

    Update music.py
    added master log

--- Primary Cubesat/PI0/music.py
+++ Primary Cubesat/PI0/music.py
@@ -1,6 +1,9 @@
 from pygame import mixer
 from time import sleep as s
 
+log = ["The music started playing at: ", "The music stopped playing at: "]
+o = open("masterLog.txt", "a+")
+
 file1 = '/home/pi/Music/danger_zone.mp3'
 file2 = '/home/pi/Music/Starman.mp3'
 file3 = '/home/pi/Music/cakes.mp3'
@@ -9,6 +12,7 @@ file5 = '/home/pi/Music/space_oddity.mp3'
 
 mixer.init()
 
+o.write(log[1] + time.strftime('%H:%M:%S') + '\n')
 mixer.music.load(file1)
 mixer.music.play()
 s(180)
@@ -27,5 +31,6 @@ s(180)
 
 mixer.music.load(file5)
 mixer.music.play()
+o.write(log[1] + time.strftime('%H:%M:%S') + '\n')
 
-print("finished")
+#print("finished")
