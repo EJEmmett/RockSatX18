ffa49d56e490f66304c50c81748fa292623395fe
sschatz1997 <s.schatz1997@gmail.com>
2018-05-30 16:57:59 -0400

    added maser file

--- Tests/laserTests.py
+++ Tests/laserTests.py
@@ -4,6 +4,7 @@ from datetime import datetime
 mini.BAUDRATE=115200
 primaryInstrument = mini.Instrument(port, 1, mode='rtu')
 primaryInstrument.write_register(4, value=75, functioncode=6)
+o = open("masterLog.txt", "a+")
 
 while 1:
     primaryPass = primaryInstrument.read_register(24, functioncode = 4)
@@ -11,4 +12,6 @@ while 1:
     if primaryPass is not 0:
         instance = ('Laser passed at: ' + datetime.now().strftime('%H:%M:%S'))
     if instance is not None:
-        print(instance)
+        #print(instance)
+		o.write(instance)
+		o.close()
