38037c6b5d862a48ebf414fbc763bb1a5ec90dd2
edemms <edemms12@gmail.com>
2018-06-08 23:44:00 -0400

    tests and cleaning

--- Tests/laser.py
+++ Tests/laser.py
@@ -1,9 +1,13 @@
 from laserTests import Iridium, Laser
+from multiprocessing import Pipe
+from time import sleep
 
 def main():
-    laser = Laser()
-    laser.measure()
-
+    parent, child = Pipe()
     iridium = Iridium()
+    laser = Laser()
+
+    laser.measure(child)
+    print(parent.recv())
     iridium.sendMessage("HI")
 main()
--- Tests/laserTests.py
+++ Tests/laserTests.py
@@ -1,16 +1,42 @@
 import minimalmodbus as mini
-from datetime import datetime
+from time import sleep
+import serial
 
-mini.BAUDRATE=115200
-primaryInstrument = mini.Instrument(port, 1, mode='rtu')
-primaryInstrument.write_register(4, value=20, functioncode=6)
-o = open("/home/pi/masterLog.txt", "a+")
+class Laser:
+    def __init__(self):
+        mini.BAUDRATE=115200
+        try:
+            self.primaryInstrument = mini.Instrument("/dev/ttyUSB0", 1, mode='rtu')
+        except:
+            self.primaryInstrument = mini.Instrument("/dev/ttyUSB1", 1, mode='rtu')
+        self.primaryInstrument.write_register(4, value=20, functioncode=6)
 
-while 1:
-    primaryPass = primaryInstrument.read_register(24, functioncode = 4)
-    instance = None
-    if primaryPass is not 0:
-        instance = ('Laser passed at: ' + datetime.now().strftime('%H:%M:%S'))
-    if instance is not None:
-        #print(instance)
-		o.write(instance)
+    def measure(self, conn):
+        primaryPass = self.primaryInstrument.read_register(24, functioncode = 4)
+        instance = None
+
+        if primaryPass is not 0:
+            instance = ("Laser passed at " + str(primaryPass))
+
+        if instance is not None:
+            conn.send(instance)
+
+class Iridium:
+    def __init__(self):
+        try:
+            self.ser = serial.Serial(port="/dev/ttyUSB1", baudrate=19200, xonxoff=True)
+        except:
+            self.ser = serial.Serial(port="/dev/ttyUSB0", baudrate=19200, xonxoff=True)
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+
+    def sendMessage(self, m):
+        returned = None
+        self.ser.write(('AT+SBDWRT= ' + m + '\r').encode())
+        sleep(.1)
+        self.ser.reset_input_buffer()
+        self.ser.write('AT+SBDIX\r'.encode())
+        sleep(.1)
+        print(self.ser.read(size=self.ser.in_waiting).decode())
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
