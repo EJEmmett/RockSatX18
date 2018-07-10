0fc0def7f69c4e3f295c783d4bf38ae8211b32ad
sschatz1997 <s.schatz1997@gmail.com>
2018-07-09 22:03:13 -0400

    time

--- Primary Cubesat/functions.py
+++ Primary Cubesat/functions.py
@@ -44,10 +44,23 @@ class Iridium:
             self.ser = serial.Serial(port="/dev/ttyUSB0", baudrate=19200, xonxoff=True)
         self.ser.reset_input_buffer()
         self.ser.reset_output_buffer()
-
-    def image_transmission(self, stream_p):
+	
+	sleep(35)
+    def broadcast(self):
+        x1 = 1
+		while x1 == 1:
+            self.ser.write(b'AT+SBDWT=Hello World                    \r') #31 Bytes
+            sleep(.1)
+            self.ser.write(b'AT+SBDIX\r')
+            sleep(.1)
+            self.ser.write(b'AT+SBDD0\r')
+			x1 = 0
+			
+	sleep(80)
+	def image_transmission(self, stream_p):
         split_stream = list(stream_p.recv_bytes())
-        while True:
+		x2 = 0
+        while x2 != 1
             if split_stream:
                 self.ser.write(("AT+SBDWT=" + bytes(split_stream[0:120]) + "\r").encode())
                 sleep(.1)
@@ -57,15 +70,8 @@ class Iridium:
                 del split_stream[0:120]
             else:
                 break
-
-    def broadcast(self):
-        while True:
-            self.ser.write(b'AT+SBDWT=Hello World                    \r') #31 Bytes
-            sleep(.1)
-            self.ser.write(b'AT+SBDIX\r')
-            sleep(.1)
-            self.ser.write(b'AT+SBDD0\r')
-
+				
+	sleep()
     def send_message(self, message):
         self.ser.write(('AT+SBDWRT=' + message + '\r').encode())
         sleep(.1)
@@ -77,7 +83,7 @@ def capture_picture(stream_c):
     camera = picamera.PiCamera()
     camera.exposure_mode = 'antishake'
 
-    sleep(2)
+    sleep(40)
     index = 0
     maximum = 135
 
