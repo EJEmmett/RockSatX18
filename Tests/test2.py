06d3cd44ed44711bb01d1214ba2445170eb15e1f
sschatz1997 <s.schatz1997@gmail.com>
2018-07-23 19:32:56 -0400

    gets the error codes

---
+++ Tests/test2.py
@@ -0,0 +1,23 @@
+from time import sleep
+import serial
+
+ser = serial.Serial(port="/dev/ttyUSB0", baudrate=19200, xonxoff=True)
+x = 0
+y = 0
+while x != 10: 
+    sleep(1)
+    ser.write("AT+SBDWT=[TEST    ]\r".encode())
+    p = ser.read(size=ser.in_waiting).decode().translate(str.maketrans( '', '', '')).split(" ")
+    print(p)
+    x += 1
+    sleep(2)
+
+    while y != 10: 
+        sleep(1)
+        p = ser.read(size=ser.in_waiting).decode().translate(str.maketrans( '', '', ',')).split(" ")
+        ser.write('AT+SBDIX\r'.encode())
+        print(p)
+        y += 1
+        sleep(2)
+        ser.write('AT+SBDD0\r'.encode())
+        sleep(1)
