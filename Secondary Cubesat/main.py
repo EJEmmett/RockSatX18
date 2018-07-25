e941e3b2fe30d0c067bbe54dd068ea995f1b38fb
sschatz1997 <s.schatz1997@gmail.com>
2018-07-25 10:50:20 -0400

    Added writing to the masterlog file

--- Secondary Cubesat/main.py
+++ Secondary Cubesat/main.py
@@ -6,14 +6,19 @@ def main():
     ser = serial.Serial(port='/dev/ttyUSB0', baudrate=19200, xonxoff=True)
     ser.reset_input_buffer()
     ser.reset_output_buffer()
+	o = open("/home/pi/masterLog.txt", "a+")
     sleep(69)
     while x != 5:
         ser.write(b'AT+SBDREG')
         x = x + 1
     while True:
         ser.write('AT+SBDWT=[BM1:Major Tom to ground control               ]\r'.encode())
+		o.write("Began to beacan")
+		o.close()
         sleep(.1)
         ser.write('AT+SBDIX\r'.encode())
+		o.write("Sent beacan")
+		o.close()
         sleep(2)
         ser.write('AT+SBDD0\r'.encode())
         sleep(.1)
