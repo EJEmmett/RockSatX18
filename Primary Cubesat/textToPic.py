614eee5cecd4a21ded81f61fbb0dd6b396a9534d
sschatz1997 <s.schatz1997@gmail.com>
2018-08-07 12:51:39 -0400

    fix

--- Primary Cubesat/textToPic.py
+++ Primary Cubesat/textToPic.py
@@ -36,32 +36,13 @@ with open("/home/sam/Downloads/SBD/picBASE64.txt") as f:
     c16 = c16[:-2]
     c17 = f.readline(17)
     c17 = c17[:-2]
-    c18 = f.readline(18)
-    c18 = c18[:-2]
-    c19 = f.readline(19)
-    c19 = c19[:-2]
-    c20 = f.readline(20)
-    c20 = c20[:-2]        
-    c21 = f.readline(21)
-    c21 = c21[:-2]
-    c22 = f.readline(22)
-    c22 = c22[:-2]
-    c23 = f.readline(23)
-    c23 = c23[:-2]
-    c24 = f.readline(24)
-    c24 = c24[:-2]
-    c25 = f.readline(25)
-    c25 = c25[:-2]
-    c26 = f.readline(26)
-    c26 = c26[:-2]
-    c27 = f.readline(27)
-    c27 = c27[:-2]
+
         
 c = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10
 c = c + c11 + c12 + c13 + c14 + c15 + c16 + c17 + c18 + c19 + c20
 c = c + c21 + c22 + c23 + c24 + c25 + c26 + c27
 c = base64.b64decode(c)
-with open("/home/sam/Downloads/pic1.jpg", "w") as pic:
+with open("/home/sam/Downloads/pic.jpg", "w") as pic:
         pic.write(c)
         pic.close()
         f.close()
